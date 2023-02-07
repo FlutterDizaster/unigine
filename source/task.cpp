f248fbfa4023b18b37082637fa4ed4764c5fb927
gopherangelo <diman1251@gmail.com>
2023-02-07 19:47:06 +0700

    init

---
+++ source/task.cpp
@@ -0,0 +1,8 @@
+// this file you need to fill
+// этот файл вам нужно заполнить
+#include "task.h"
+
+void Task::checkVisible(const std::vector<unit> &input_units, std::vector<int> &result)
+{
+	// fill vector result
+}
